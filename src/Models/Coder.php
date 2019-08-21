7b90a478fc8780d141534fed348441842de40cff
Sergi Virgili <svirgilif@gmail.com>
2019-08-21 16:52:39 +0200

    test Coder first time

--- src/Models/Coder.php
+++ src/Models/Coder.php
@@ -1,6 +1,38 @@
-<?
+<?php
 namespace Models;
 
-class Coder {
+class Coder 
+{
+    private $id;
+    private $name = "";
+    private $dead = false;
 
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+
+    }
+
+    function setName($name){
+        $this->name = $name;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function __construct ($id = null, $name = "", $dead= false) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->dead = $dead;
+    }
 }
+
+
+
--- tests/CoderTest.php
+++ tests/CoderTest.php
@@ -1,12 +1,55 @@
 <?php
 
-use app\Coder;
+use Models\Coder;
 
 use PHPUnit\Framework\TestCase;
 
 class CoderTest extends TestCase
-{
+{    
+    public function testeaSiDevuelveUnIdInt()
+    {
+        //Definir escenario del test
+        $obj = new Coder();
+        $obj->setId(2);
+        //Ejecutar escenario del test
+        $response = $obj->getId();
+        //Hacemos assertion del test
+        $this->assertIsInt($response);
 
- 
+    }
+
+    public function testeaElNumeroDeIdCoincide()
+    {
+        //Definir escenario del test
+        $obj = new Coder();
+        $obj->setId(2);
+        //Ejecutar escenario del test
+        $response = $obj->getId();
+        //Hacemos assertion del test
+        $this->assertEquals(2, $response);
+    }
+
+    public function testeaSiElNombreEsString()
+    {
+        //Definir escenario del test
+        $nuria = new Coder();
+        $respuesta = $nuria->getName();
+        $this->assertIsString($respuesta); 
+    }
+    
+    function testeaQueElNombreSeaIgualAlQueLeDiga()
+    {
+        $laia = new Coder();
+        $laia->setName('laia');
+
+
+        $nombre =  $laia -> getName();
+        
+        $this-> assertEquals('laia' ,$nombre);
+
+    }
+
+
+    
 
 }
