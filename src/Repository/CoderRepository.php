4281b5ff1a92e77426dc1920d5603bd621834986
colmanfranco <colmanfm@gmail.com>
2019-08-23 10:46:54 +0200

    funcion selectAll

--- src/Repository/CoderRepository.php
+++ src/Repository/CoderRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 class CoderRepository
 {
 
-    private $table = 'coders';
+    private $table = 'coder';
     private $conexion;
     private $server = "localhost";
     private $user = "root";
@@ -14,24 +14,39 @@ class CoderRepository
 
     function connectDB()
     {
-        $conexion = mysqli_connect($this->server, $this->user, $this->password, $this->db);
-        
-        if(!$conexion)
+        $this->conexion = mysqli_connect($this->server, $this->user, $this->password) or die ("No se ha podido conectar al servidor");
+
+        $dbSelected = mysqli_select_db($this->conexion, $this->db) or die ("No se ha podido leer la base de datos");
+
+        if(!$this->conexion)
         {
             $response = "No se ha podido conectar";
             echo $response;
         }
 
         $response = "Conexion Exitosa";
+        echo $response;
         return $response;
     }
 
     function selectAll()
     {
-        $query = 'SELECT * FROM coders';
-        $respuesta = mysqli_query($this->conexion, $query);
-        
-        echo $respuesta;
+        $conn = $this->conexion;
+        $query = "SELECT * FROM coder";
+        $respuestas = mysqli_query($conn, $query);
+
+        $arrayCoders = array();
+        while ($row = mysqli_fetch_array( $respuestas ))
+        {
+            $arrayCoders[] = array 
+            (
+            "id" => $row['id_coder'],
+            "coderName" => $row['name_coder'],
+            "state"=> $row['dead']
+        );
+        };
+
+        echo $arrayCoders[3][coderName];
     }
 
     function selectById($id)
